067b2d06c1b612889d6bf0a653e9ced8dd847379
emiliano <emiliano@mildewyPrawn.fciencias.unam.mx>
2018-03-04 00:09:29 -0600

    vL, neg, eq, fnn

--- Practicas/Practica2/src/Practica2.hs
+++ Practicas/Practica2/src/Practica2.hs
@@ -13,8 +13,10 @@
 module Practica2 where
 --declaracion de los tipo?
 
-data Var = A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z deriving (Show, Eq, Ord)
+data Var = A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z
+         deriving (Show, Eq, Ord)
 data Formula = Prop Var
+             --Agregar V,F para True y False (?)
              |Neg Formula
              |Formula :&: Formula
              |Formula :|: Formula
@@ -28,42 +30,81 @@ infixr 7 :=>:
 infixl 8 :<=>:
 
 
---Una función recursiva que recibe una fórmula y devuelve el conjunto (lista sin repeticiones) de
---variables que hay en la fórmula. HINT: Elimina las repeticiones utilizando una función de tu
---práctica anterior.
+--Una función recursiva que recibe una fórmula y devuelve el conjunto (lista sin
+--  repeticiones) de variables que hay en la fórmula. HINT: Elimina las
+-- repeticiones utilizando una función de tu práctica anterior.
 varList :: Formula -> [Var]
-varList = error "solo es para que interprete"
+varList (Prop p) = [p]
+varList (Neg p) = repeticiones(varList(p))
+varList (p :&: q) = repeticiones(varList(p) ++ varList(q))
+varList (p :|: q) = repeticiones(varList(p) ++ varList(q))
+varList (p :=>: q) = repeticiones(varList(p) ++ varList(q))
+varList (p :<=>: q) = repeticiones(varList(p) ++ varList(q))
 
 --Una función que recibe una fórmula y devuelve su negación.
 negacion :: Formula -> Formula
-negacion = error "solo es para que interprete"
+negacion (Prop p) = (Neg (Prop p))
+negacion (Neg p) = (p)
+negacion (p :&: q) = (negacion p) :|: (negacion q)
+negacion (p :|: q) = (negacion p) :&: (negacion q)
+negacion (p :=>: q) = p :&: (negacion q)
+negacion (p :<=>: q) =(p :&: (negacion q)) :|: (q :&: (negacion p))
 
 --Una función que recibe una fórmula y elimina implicaciones y equivalencias.
 equivalencia :: Formula -> Formula
-equivalencia = error "solo es para que interprete"
+equivalencia (Prop p) = (Prop p)
+equivalencia (Neg p) = (negacion (equivalencia p))
+equivalencia (p :&: q) = (equivalencia p) :&: (equivalencia q)
+equivalencia (p :|: q) = (equivalencia p) :|: (equivalencia q)
+equivalencia (p :=>: q) = (negacion (equivalencia p)) :|: (equivalencia q)
+equivalencia (p :<=>: q) = ((negacion(equivalencia p)) :|: (equivalencia q))
+                           :&: ((negacion(equivalencia q)) :|: (equivalencia p))
 
---Una función recursiva que recibe una fórmula proposicional y una lista de parejas de variables
---proposicionales. La función debe sustituir todas las presencias de variables en la fórmula por la
---pareja ordenada que le corresponde en la lista.
+--Una función recursiva que recibe una fórmula proposicional y una lista de
+-- parejas de variables proposicionales. La función debe sustituir todas las
+-- presencias de variables en la fórmula por la pareja ordenada que le
+-- corresponde en la lista.
 sustituye :: Formula -> [(Var,Var)] -> Formula
 sustituye = error "soloes para que interprete"
 
---Una función recursiva que recibe una fórmula y una lista de parejas ordenadas de variables con
---estados (True y False) y evalua la fórmula asignando el estado que le corresponde a cada variable.
---Si existe alguna variable que no tenga estado asignado y sea necesaria para calcular el valor de
---la proposición, muestra el error: “No todas las variables están definidas"
+--Una función recursiva que recibe una fórmula y una lista de parejas ordenadas
+-- de variables con estados (True y False) y evalua la fórmula asignando el
+-- estado que le corresponde a cada variable. Si existe alguna variable que no
+-- tenga estado asignado y sea necesaria para calcular el valor de la
+-- proposición, muestra el error: “No todas las variables están definidas"
 interp :: Formula -> [(Var,Bool)] -> Bool
 interp = error "solo es para que interprete"
 
---Una función que recibe una fórmula y devuelve la fórmula en Forma normal negativa. Deci-
---mos que una fórmula ψ está en forma normal negativa si y sólo si en ψ las negaciones quedan
---únicamente frente a fórmulas atómicas y no hay presencias de conectivo de implicación, ni equi-
---valencia.
+--Una función que recibe una fórmula y devuelve la fórmula en Forma normal
+-- negativa. Decimos que una fórmula ψ está en forma normal negativa si y sólo
+-- si en ψ las negaciones quedan únicamente frente a fórmulas atómicas y no hay
+-- presencias de conectivo de implicación, ni equivalencia.
 fnn :: Formula -> Formula
-fnn = error "solo es para que interprete"
+fnn (Prop p) = Prop p
+fnn (Neg p) = negacion (fnn (p))
+fnn (p :&: q) = (fnn p :&: fnn q)
+fnn (p :|: q) = fnn p :|: fnn q
+fnn (p :=>: q) = negacion (fnn p) :|: fnn q
+fnn (p :<=>: q) =(negacion p :|: q) :&: (negacion q :|: p) 
 
---aun no lo he leido
-fac :: Formula -> Formula
-fac = error "esto solo es para que interprete"
+--Función recursiva que permite expresar cualquier fórmula proposicional como
+-- una conjunción de disyunciones llamadas 'clausulas'.
+fnc :: Formula -> Formula
+fnc (Prop p) = Prop p
+fnc (Neg p) = negacion (fnc (fnn p))
+fnc (p :&: q) = (fnc (fnn p) :&: fnc (fnn q))
+fnc (p :|: q) = fnc (fnn p) :|: fnc (fnn q)
+fnc (p :=>: q) = negacion (fnc (fnn p)) :|: fnc (fnn q)
+fnc (p :<=>: q) =(negacion p :|: q) :&: (negacion q :|: p) 
 
---
+--fnc = error "esto solo es para que interprete"
+
+----------------------------------------------------------------------
+--                        FUNCIONES AUXILIARES                      --
+----------------------------------------------------------------------
+
+repeticiones :: (Eq a) => [a] -> [a]
+repeticiones [] = []
+repeticiones (x:xs) = if(elem x xs == True)
+                      then repeticiones xs
+                      else [x] ++ repeticiones xs
